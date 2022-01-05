0bc762f7debe51cf2db731643928be164565db2f
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2022-01-04 16:39:56 -0800

    v0.0.0.0.1 - PoC working; identifies count of lines added / deleted from the git staged and unstaged files.

--- src/main/java/com/chriscarini/jetbrains/locchangecountdetector/LOCCountWidgetText.java
+++ src/main/java/com/chriscarini/jetbrains/locchangecountdetector/LOCCountWidgetText.java
@@ -1,10 +1,21 @@
 package com.chriscarini.jetbrains.locchangecountdetector;
 
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.event.BulkAwareDocumentListener;
+import com.intellij.openapi.editor.event.CaretListener;
+import com.intellij.openapi.editor.event.EditorEventMulticaster;
+import com.intellij.openapi.editor.event.SelectionListener;
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.StatusBarWidget;
+import com.intellij.openapi.wm.impl.status.EditorBasedWidget;
 import com.intellij.util.Consumer;
+import com.intellij.util.ui.update.MergingUpdateQueue;
+import com.intellij.util.ui.update.Update;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -20,14 +31,18 @@ import java.awt.event.MouseEvent;
 // 7: Change the review time to CRL
 // 8: Add tooltip to the status bar showing the comment for review time.
 
-public class LOCCountWidgetText implements StatusBarWidget, StatusBarWidget.TextPresentation {
+public class LOCCountWidgetText extends EditorBasedWidget implements StatusBarWidget, StatusBarWidget.TextPresentation,
+        BulkAwareDocumentListener.Simple, CaretListener, SelectionListener {
 
     public static final String ID = "LoCCounter";
-    private final Project project;
 
-    public LOCCountWidgetText(@NotNull Project project) {
-        this.project = project;
+    private MergingUpdateQueue myQueue;
+    private @NlsContexts.Label String myText;
+
+    protected LOCCountWidgetText(@NotNull Project project) {
+        super(project);
     }
+
 
     @Override
     public @NonNls @NotNull String ID() {
@@ -36,18 +51,22 @@ public class LOCCountWidgetText implements StatusBarWidget, StatusBarWidget.Text
 
     @Override
     public void install(@NotNull StatusBar statusBar) {
-
+        super.install(statusBar);
+        this.myQueue = new MergingUpdateQueue("PositionPanel", 100, true, null, this);
+        EditorEventMulticaster multicaster = EditorFactory.getInstance().getEventMulticaster();
+        multicaster.addCaretListener(this, this);
+        multicaster.addSelectionListener(this, this);
+        multicaster.addDocumentListener(this, this);
     }
 
     @Override
     public void dispose() {
-
+        Disposer.dispose(this);
     }
 
     @Override
     public @NotNull @NlsContexts.Label String getText() {
-        LoCService myService = LoCService.getInstance(project);
-        return String.format("%d lines of code changed; %s files changed", myService.getChangeCount(), myService.getFileCount());
+        return this.myText == null ? "" : this.myText;
     }
 
     @Override
@@ -65,9 +84,37 @@ public class LOCCountWidgetText implements StatusBarWidget, StatusBarWidget.Text
         return null;
     }
 
-
     @Override
     public @Nullable Consumer<MouseEvent> getClickConsumer() {
         return null;
+    }
+
+    @Override
+    public void afterDocumentChange(@NotNull Document document) {
+        this.updateChangeText();
+    }
+
+    @Override
+    public void selectionChanged(@NotNull FileEditorManagerEvent event) {
+        this.updateChangeText();
+    }
+
+
+    private void updateChangeText() {
+        myQueue.queue(Update.create(this, () -> {
+            String newText = this.getChangeText();
+            if (newText.equals(myText)) return;
+
+            myText = newText;
+            if (myStatusBar != null) {
+                myStatusBar.updateWidget(ID());
+            }
+        }));
+    }
+
+    @NotNull
+    private String getChangeText() {
+        final LoCService myService = LoCService.getInstance(this.myProject);
+        return String.format("%d lines of code changed; %s files changed", myService.getChangeCount(), myService.getFileCount());
     }
 }
