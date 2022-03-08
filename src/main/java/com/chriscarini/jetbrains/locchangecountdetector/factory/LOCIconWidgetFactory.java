6fa810f6b5d49382bdf325c5d7425b67ff5a62de
Chris Carini <6374067+ChrisCarini@users.noreply.github.com>
2022-03-08 10:19:15 -0800

    Add Widget Icon Factory (#15) (#20)

---
+++ src/main/java/com/chriscarini/jetbrains/locchangecountdetector/factory/LOCIconWidgetFactory.java
@@ -0,0 +1,28 @@
+package com.chriscarini.jetbrains.locchangecountdetector.factory;
+
+import com.chriscarini.jetbrains.locchangecountdetector.widget.LOCWidgetIcon;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.StatusBarWidget;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public class LOCIconWidgetFactory implements LOCBaseWidgetFactory {
+
+    @Override
+    public @NonNls @NotNull String getId() {
+        return LOCWidgetIcon.ID;
+    }
+
+    @Override
+    public @NotNull StatusBarWidget createWidget(@NotNull Project project) {
+        return new LOCWidgetIcon(project);
+    }
+
+//    @Override
+//    public boolean isAvailable(@NotNull Project project) {
+//        // TODO(ChrisCarini) - We could consider showing the icon only if the change size exceeds a certain limit.
+//        return ProjectLevelVcsManager.getInstance(project).hasActiveVcss() && LoCService.getInstance(project).getChangeCount() >= 300;
+//    }
+
+}
+
