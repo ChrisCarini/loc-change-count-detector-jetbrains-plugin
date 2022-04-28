0212753016b0309dc3117f696e9e0a103db7109b
Chris Carini <6374067+ChrisCarini@users.noreply.github.com>
2022-04-28 01:44:14 -0700

    Add custom names for each widget (icon & text), extract into message bundle (#39)

--- src/main/java/com/chriscarini/jetbrains/locchangecountdetector/factory/LOCPerCommitWidgetFactory.java
+++ src/main/java/com/chriscarini/jetbrains/locchangecountdetector/factory/LOCPerCommitWidgetFactory.java
@@ -1,5 +1,6 @@
 package com.chriscarini.jetbrains.locchangecountdetector.factory;
 
+import com.chriscarini.jetbrains.locchangecountdetector.messages.Messages;
 import com.chriscarini.jetbrains.locchangecountdetector.widget.LOCCountWidgetText;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.StatusBarWidget;
@@ -7,6 +8,10 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 public class LOCPerCommitWidgetFactory implements LOCBaseWidgetFactory {
+
+    public @NotNull String getDisplayName() {
+        return Messages.message("loc.widget.factory.text.display.name");
+    }
 
     @Override
     public @NonNls @NotNull String getId() {
