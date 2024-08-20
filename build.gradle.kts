531ab7f9294256fa012344572764cc99f1d2860d
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2024-08-20 05:15:40 -0700

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
 plugins {
     id("build-standard-jetbrains-plugin-build")
 }
+
+// Configure "Bundling API Sources" - https://plugins.jetbrains.com/docs/intellij/bundling-plugin-openapi-sources.html#bundling-api-sources-in-gradle-build-script
+tasks {
+    val createOpenApiSourceJar by registering(Jar::class) {
+        // Java sources
+        from(sourceSets.main.get().java) {
+            include("**/com/chriscarini/jetbrains/locchangecountdetector/**/*.java")
+        }
+        destinationDirectory.set(layout.buildDirectory.dir("libs"))
+        archiveClassifier.set("src")
+    }
+
+    buildPlugin {
+        dependsOn(createOpenApiSourceJar)
+        from(createOpenApiSourceJar) { into("lib/src") }
+    }
+}
